8f9154657e9b09c4d8950357d4dea2bc856cf7d9
TungNV <ngotung180301@gmail.com>
2023-03-06 22:11:24 +0700

    update

--- src/components/page/RecruitManag.vue
+++ src/components/page/RecruitManag.vue
@@ -29,35 +29,113 @@
                 <div>
                   <img class="image" src="../../assets/img/anh1.jpg" alt="">
                 </div>
-                <h3 class="header-rec">Giảng viên môn Web</h3>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
                 <div class="title-name">Đại học thủy lợi</div>
                 <div>Chứng chỉ đào tạo: Thạc sĩ</div>
-                <div class="icon">
-                  <i class="fa fa-trash-alt"></i>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
                 </div>
               </div>
             </div>
-            <div class="item-recruit"></div>
-            <div class="item-recruit"></div>
+            <div class="item-recruit">
+              <div class="body-rec">
+                <div>
+                  <img class="image" src="../../assets/img/anh1.jpg" alt="">
+                </div>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
+                <div class="title-name">Đại học thủy lợi</div>
+                <div>Chứng chỉ đào tạo: Thạc sĩ</div>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
+                </div>
+              </div>
+            </div>
+            <div class="item-recruit">
+              <div class="body-rec">
+                <div>
+                  <img class="image" src="../../assets/img/anh1.jpg" alt="">
+                </div>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
+                <div class="title-name">Đại học thủy lợi</div>
+                <div>Chứng chỉ đào tạo: Thạc sĩ</div>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
+                </div>
+              </div>
+            </div>
+            <div class="item-recruit">
+              <div class="body-rec">
+                <div>
+                  <img class="image" src="../../assets/img/anh1.jpg" alt="">
+                </div>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
+                <div class="title-name">Đại học thủy lợi</div>
+                <div>Chứng chỉ đào tạo: Thạc sĩ</div>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
+                </div>
+              </div>
+            </div>
+            <div class="item-recruit">
+              <div class="body-rec">
+                <div>
+                  <img class="image" src="../../assets/img/anh1.jpg" alt="">
+                </div>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
+                <div class="title-name">Đại học thủy lợi</div>
+                <div>Chứng chỉ đào tạo: Thạc sĩ</div>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
+                </div>
+              </div>
+            </div>
+            <div class="item-recruit">
+              <div class="body-rec">
+                <div>
+                  <img class="image" src="../../assets/img/anh1.jpg" alt="">
+                </div>
+                <h3 class="header-h3">Giảng viên môn Web</h3>
+                <div class="title-name">Đại học thủy lợi</div>
+                <div>Chứng chỉ đào tạo: Thạc sĩ</div>
+                <div class="btn-wrap">
+                  <div class="btnEdit">Edit</div>
+                  <div class="btnDelete">Delete</div>
+                </div>
+              </div>
+            </div>
         </div>
     </div>
     </div>
    
 </template>
 <style scoped>
+.container {
+  /* height: 50vh; */
+}
+.body-recruit {
+  margin: 0 auto;
+  max-width: 1000px;
+}
 .item-row{
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    gap: 20px;
+    /* justify-content: space-between; */
+    /* flex-wrap: wrap; */
 }
 .item-recruit{
     border-radius: 4px;
-    width: 350px;
-    height: 350px;
+    /* width: 350px; */
     background-color: #fff;
     padding: 8px;
+    /* overflow-y: hidden; */
 }
 .image {
-  margin-top: 10px;
   border-radius: 4px;
   width: 100%;
   height: 100%;
@@ -65,9 +143,35 @@
 .title-name {
   margin-bottom: 10px;
 }
-.icon {
-  
+.btn-wrap{
+  display: flex;
+  justify-content: flex-end;
+  column-gap: 10px;
+  margin-top: 10px;
 }
+.btnEdit,
+.btnDelete{
+  border: 1px solid #ccc;
+  padding: 6px 8px;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 767.98px) {
+  .item-row{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+}
+}
+@media screen and (max-width: 600px) {
+  .item-row{
+    display: grid;
+    grid-template-columns: 1fr;
+}
+}
+
+
+
 </style>
 
 <script>
