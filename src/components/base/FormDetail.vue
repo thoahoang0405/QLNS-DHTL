cea4cbf4d414ea10808e7d54d3c59756277348a7
TungNV <ngotung180301@gmail.com>
2023-03-07 22:50:26 +0700

    validate

--- src/components/base/FormDetail.vue
+++ src/components/base/FormDetail.vue
@@ -1,6 +1,6 @@
 <template>
      <div id="form">
-        <div class="form">
+        <form class="form">
             <div class="form-top">
                 <div class="form-title">
                     <h3 class="title">Thêm/sửa thông tin sinh viên</h3>
@@ -11,19 +11,21 @@
                 <div class="column">
                     <div class="input__box">
                         <label for="">Mã sinh viên</label>
-                        <input type="text">
+                        <input @blur="validate()" class="masv" type="text" v-model="desc.masv">
+                        <div class="invalid-feedback" v-if="errors.masv">{{ errors.masv }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Tên sinh viên</label>
-                        <input type="text">
+                        <input @blur="validate()" class="ten" type="text" v-model="desc.ten">
+                        <div class="invalid-feedback" v-if="errors.ten">{{ errors.ten }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Giới tính</label>
                         <div class="radio__box">
                             <input type="radio" name="gt">
-                            <label for="">Nam</label>
+                            <label class="nam" for="">Nam</label>
                             <input type="radio" name="gt">
-                            <label for="">Nữ</label>
+                            <label class="nu" for="">Nữ</label>
                         </div>
                     </div>
                 </div>
@@ -31,64 +33,78 @@
                 <div class="column">
                     <div class="input__box">
                         <label for="">Ngày sinh</label>
-                        <input type="text">
+                        <input @blur="validate()" class="ngaysinh" type="date" v-model="desc.ngaysinh">
+                        <div class="invalid-feedback" v-if="errors.ngaysinh">{{ errors.ngaysinh }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Khoa</label>
-                        <input type="text">
+                        <input @blur="validate()" class="khoa" type="text" v-model="desc.khoa">
+                        <div class="invalid-feedback" v-if="errors.khoa">{{ errors.khoa }}</div>
                     </div>
                 </div>
                 <div class="column">
                     <div class="input__box">
-                        <label for="">Cmnd/cccd</label>
-                        <input type="text">
+                        <label for="">Cmnd/cccd</label> 
+                        <input @blur="validate()" class="cmnd" type="text" v-model="desc.cmnd">
+                        <div class="invalid-feedback" v-if="errors.cmnd">{{ errors.cmnd }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Ngày cấp</label>
-                        <input type="text">
+                        <input @blur="validate()" class="ngaycap" type="date" v-model="desc.ngaycap">
+                        <div class="invalid-feedback" v-if="errors.ngaycap">{{ errors.ngaycap }}</div>
                     </div>
                 </div>
                 <div class="input__box">    
                     <label for="">Nơi cấp</label>
-                    <input type="text">
+                    <input @blur="validate()" class="noicap" type="text" v-model="desc.noicap">
+                        <div class="invalid-feedback" v-if="errors.noicap">{{ errors.noicap }}</div>
                 </div>
                 <div class="column">
                     <div class="input__box">
                         <label for="">Email</label>
-                        <input type="text">
+                        <input @blur="validate()" class="email" type="text" v-model="desc.email">
+                        <div class="invalid-feedback" v-if="errors.email">{{ errors.email }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Số điện thoại</label>
-                        <input type="text">
+                        <input @blur="validate()" class="sodt" type="text" v-model="desc.sodt">
+                        <div class="invalid-feedback" v-if="errors.sodt">{{ errors.sodt }}</div>
                     </div>
                 </div>
                 <div class="input__box">    
                     <label for="">Địa chỉ</label>
-                    <input type="text">
+                    <input @blur="validate()" class="diachi" type="date" v-model="desc.diachi">
+                    <div class="invalid-feedback" v-if="errors.diachi">{{ errors.diachi }}</div>
                 </div>
                 <div class="column column-s">
                     <div class="input__box">
                         <label for="">Chương trình đào tạo</label>
-                        <input type="text">
+                        <input @blur="validate()" class="daotao" type="text" v-model="desc.daotao">
+                        <div class="invalid-feedback" v-if="errors.daotao">{{ errors.daotao }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Ngày nhập học</label>
-                        <input type="text">
+                        <input @blur="validate()" class="ngaynhaphoc" type="date" v-model="desc.ngaynhaphoc">
+                        <div class="invalid-feedback" v-if="errors.ngaynhaphoc">{{ errors.ngaynhaphoc }}</div>
                     </div>
                     <div class="input__box">
                         <label for="">Ngày dự kiến tốt nghiệp</label>
-                        <input type="text">
+                        <input @blur="validate()" class="ngaytotnghiep" type="date" v-model="desc.ngaytotnghiep">
+                        <div class="invalid-feedback" v-if="errors.ngaytotnghiep">{{ errors.ngaytotnghiep }}</div>
                     </div>
                 </div>
             </div>
             <div class="form-bottom">
                 <div class="btn btn-cancel">Hủy</div>
-                <div class="btn btn-save">Lưu</div>
+                <div class="btn btn-save" @click="save()">Lưu</div>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 <style>
+.invalid-feedback {
+    color: red;
+}
 #form {
     background-color: rgba(0, 0, 0, 0.2);
     z-index: 3;
@@ -119,7 +135,7 @@
     margin: 25px auto;
     border-radius: 8px;
     width: 595px;
-    display: none;
+    /* display: none; */
 }
     .form-Wrap {
         padding: 0 20px;
@@ -192,5 +208,136 @@
     height: 20px;   
    }
   }
-  
 </style>
+<script>
+    export default {
+        data(){
+            return {
+                errors: {
+                    masv: '',
+                    ten: '',
+                    ngaysinh: '',
+                    khoa: '',
+                    cmnd: '',
+                    ngaycap: '',
+                    noicap: '',
+                    email: '',
+                    sodt: '',
+                    diachi: '',
+                    daotao: '',
+                    ngaynhaphoc: '',
+                    ngaytotnghiep: '',
+                },
+                desc: {
+                    masv: '',
+                    ten: '',
+                    ngaysinh: '',
+                    khoa: '',
+                    cmnd: '',
+                    ngaycap: '',
+                    noicap: '',
+                    email: '',
+                    sodt: '',
+                    diachi: '',
+                    daotao: '',
+                    ngaynhaphoc: '',
+                    ngaytotnghiep: '',
+                }
+            }
+        },
+        methods: {
+            validate(){
+                let isValid = true;
+
+                this.errors = {
+                    masv: '',
+                    ten: '',
+                    ngaysinh: '',
+                    khoa: '',
+                    cmnd: '',
+                    ngaycap: '',
+                    noicap: '',
+                    email: '',
+                    sodt: '',
+                    diachi: '',
+                    daotao: '',
+                    ngaynhaphoc: '',
+                    ngaytotnghiep: '',
+                }
+                if(!this.desc.masv) {
+                    this.errors.masv = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.ten) {
+                    this.errors.ten = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.ngaysinh) {
+                    this.errors.ngaysinh = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.sodt) {
+                    this.errors.sodt = "Không được để trống!";
+                    isValid = false;
+                } else if(!this.isNumber(this.desc.sodt)) {
+                    this.errors.sodt = "Yêu cầu nhập số";
+                    isValid = false;
+                }
+                if(!this.desc.khoa) {
+                    this.errors.khoa = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.cmnd) {
+                    this.errors.cmnd = "Không được để trống!";
+                    isValid = false;
+                }else if(!this.isNumber(this.desc.cmnd)) {
+                    this.errors.cmnd = "Yêu cầu nhập số";
+                    isValid = false;
+                }
+                if(!this.desc.ngaycap) {
+                    this.errors.ngaycap = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.noicap) {
+                    this.errors.noicap = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.email) {
+                    this.errors.email = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.diachi) {
+                    this.errors.diachi = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.daotao) {
+                    this.errors.daotao = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.ngaynhaphoc) {
+                    this.errors.ngaynhaphoc = "Không được để trống!";
+                    isValid = false;
+                }
+                if(!this.desc.ngaytotnghiep) {
+                    this.errors.ngaytotnghiep = "Không được để trống!";
+                    isValid = false;
+                }
+                return isValid
+            },
+            isNumber(value) {
+                return /^\d*$/.test(value);
+            },
+            save(){
+                this.validate()
+                console.log(this.validate())
+            },
+            // onclick(){
+            //     this.save();
+            // }
+        },
+        created() {
+            // this.save()
+            //gọi chỗ này là khi mở lên nó gọi hàm kia luôn k cần click gì
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
+// import 'boostsrap/dict/css/bootstrap-grid.min.css'
+// import 'boostsrap/dict/css/bootstrap-utilities.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-utilities.min.css'
 const app =createApp(App)
 app.use(router);
 app.mount("#app");
